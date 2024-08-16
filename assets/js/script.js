099862e6530544a5e6aad3b3db5beb574f343f28
masirul-codify <masirulislam80@gmail.com>
2024-08-17 05:16:42 +0600

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,162 @@
+
+(function ($) {
+    "use strict";
+
+    $(document).ready(function () {
+     
+
+        
+
+        /*-----------------------------------
+          01. Mobile Menu  
+        -----------------------------------*/   
+        $('#mobile-menu').meanmenu({
+            meanMenuContainer: '.mobile-menu',
+            meanScreenWidth: "1199",
+            meanExpand: ['<i class="far fa-plus"></i>'],
+        });
+
+
+
+        /*-----------------------------------
+          02. Sidebar Toggle  
+        -----------------------------------*/   
+        $(".offcanvas__close,.offcanvas__overlay").on("click", function () {
+            $(".offcanvas__info").removeClass("info-open");
+            $(".offcanvas__overlay").removeClass("overlay-open");
+        });
+        $(".sidebar__toggle").on("click", function () {
+            $(".offcanvas__info").addClass("info-open");
+            $(".offcanvas__overlay").addClass("overlay-open");
+        });
+
+
+
+        /*-----------------------------------
+          03. Body Overlay 
+        -----------------------------------*/  
+        $(".body-overlay").on("click", function () {
+            $(".offcanvas__area").removeClass("offcanvas-opened");
+            $(".df-search-area").removeClass("opened");;
+            $(".body-overlay").removeClass("opened");
+        });
+ 
+
+
+        /*-----------------------------------
+          04. Sticky Header 
+        -----------------------------------*/    
+        $(window).scroll(function () {
+            if ($(this).scrollTop() > 250) {
+                $("#header-sticky").addClass("sticky");
+            } else {
+                $("#header-sticky").removeClass("sticky");
+            }
+        });
+
+    
+
+        //>> Search Popup Start <<//
+        const $searchWrap = $(".search-wrap");
+        const $navSearch = $(".nav-search");
+        const $searchClose = $("#search-close");
+
+        $(".search-trigger").on("click", function (e) {
+            e.preventDefault();
+            $searchWrap.animate({ opacity: "toggle" }, 500);
+            $navSearch.add($searchClose).addClass("open");
+        });
+
+        $(".search-close").on("click", function (e) {
+            e.preventDefault();
+            $searchWrap.animate({ opacity: "toggle" }, 500);
+            $navSearch.add($searchClose).removeClass("open");
+        });
+
+        function closeSearch() {
+            $searchWrap.fadeOut(200);
+            $navSearch.add($searchClose).removeClass("open");
+        }
+
+        $(document.body).on("click", function (e) {
+            closeSearch();
+        });
+
+        $(".search-trigger, .main-search-input").on("click", function (e) {
+            e.stopPropagation();
+        });
+
+        
+        /*-----------------------------------
+           05. Back to top    
+        -----------------------------------*/  
+        $(window).scroll(function () {
+            if ($(this).scrollTop() > 20) {
+                $("#back-top").addClass("show");
+            } else {
+                $("#back-top").removeClass("show");
+            }
+        });
+        $("#back-top").click(function () {
+            $("html, body").animate({ scrollTop: 0 }, 800);
+            return false;
+        });
+     
+
+        
+
+        /*-----------------------------------
+            06. Mouse Cursor    
+        -----------------------------------*/   
+        function mousecursor() {
+            if ($("body")) {
+                const e = document.querySelector(".cursor-inner"),
+                    t = document.querySelector(".cursor-outer");
+                let n,
+                    i = 0,
+                    o = !1;
+                (window.onmousemove = function(s) {
+                    o ||
+                        (t.style.transform =
+                            "translate(" + s.clientX + "px, " + s.clientY + "px)"),
+                        (e.style.transform =
+                            "translate(" + s.clientX + "px, " + s.clientY + "px)"),
+                        (n = s.clientY),
+                        (i = s.clientX);
+                }),
+                $("body").on("mouseenter", "a, .cursor-pointer", function() {
+                    e.classList.add("cursor-hover");
+                    t.classList.add("cursor-hover");
+                }),
+                $("body").on("mouseleave", "a, .cursor-pointer", function() {
+                    ($(this).is("a") && $(this).closest(".cursor-pointer").length) ||
+                    (e.classList.remove("cursor-hover"),
+                        t.classList.remove("cursor-hover"));
+                }),
+                (e.style.visibility = "visible"),
+                (t.style.visibility = "visible");
+            }
+        }
+        $(function() {
+            mousecursor();
+        });
+
+
+
+    }); // End Document Ready Function
+
+
+
+
+        function loader() {
+            $(window).on('load', function () {
+                // Animate loader off screen
+                $(".preloader").addClass('loaded');
+                $(".preloader").delay(600).fadeOut();
+            });
+        }
+
+        loader();
+
+
+    })(jQuery); // End jQuery
